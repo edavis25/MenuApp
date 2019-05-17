0c21885079c158b8b01427f04ed1d02b582c2c66
Eric Davis <davis.25811@gmail.com>
2019-05-17 02:04:05 -0400

    Remove unused methods from AddonGroupController

--- app/Http/Controllers/Admin/AddonGroupController.php
+++ app/Http/Controllers/Admin/AddonGroupController.php
@@ -11,26 +11,6 @@ use Illuminate\Http\Request;
 class AddonGroupController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\AddonGroupRequest  $request
@@ -48,28 +28,6 @@ class AddonGroupController extends Controller
         ]));
 
         return redirect()->back();
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
